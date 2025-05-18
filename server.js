ec736ecb463f1eaba668072bfce8c8a481c11145
country21402 <Joeluisvillalobos@gmail.com>
2025-05-18 12:33:32 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Sistema de afiliados funcionando 🚀');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
